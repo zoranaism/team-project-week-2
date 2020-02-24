e90da437fe5c579abaaf8f87e7e2042d70715e5f
Jeroen <Jeroenbruinsma@gmail.com>
2020-02-24 16:34:09 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Jeroen is coding like
-          crazy today
+          crazy today im notsss
         </p>
         <a
           className="App-link"
