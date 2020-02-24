9178db096d68f6fb30375968ad472c086aeb9cc1
Tonic78 <guymertz@gmaiL.com>
2020-02-24 17:45:39 +0100

    Add footer component, fix bug

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Feed from "./Feed/Feed";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Feed />
+      <Footer />
     </div>
   );
 }
