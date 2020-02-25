c8f069644aea628f6583d573393d6de984971316
zoranaism <lazareviczorana@gmail.com>
2020-02-25 08:50:26 +0100

    Feed style, feed badges, jumbotron

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,5 +1,39 @@
 import React from "react";
 
-export default function Feed() {
-  return <div>FEED WILL GO HERE</div>;
+export default function Feed(props) {
+  return (
+    <div className="col-12 col-sm-6">
+
+    
+      <div class="article card mb-4">
+        <img class="card-img-top" src="https://picsum.photos/300/200" alt="" />
+
+        <div class="card-body">
+          
+            {props.styles.map(style => {
+              return <span class="badge badge-info mr-2">{style}</span>
+            })}
+
+            <h2 class="card-title">{props.name}</h2>
+            <p class="blockquote">
+            Written by: <cite title="Source Title">{props.writer}</cite> 
+            </p>
+            <p class="card-subtitle">Location: {props.location}</p>
+
+            <p>Time of the Year: {props.season}<br />
+            Date: {props.date}<br />
+            Elevation: {props.elevation}</p>
+            <p class="card-text">About: {props.text}</p>  
+        </div>
+        
+        
+          <div class="card-footer">
+          <a href="#" class="btn btn-primary mr-2">{props.like ? "yes, liked" : "not liked"}</a>
+          <a href="ReadLink:" class="btn btn-primary">Read the article</a>
+          </div>
+
+      </div>
+    </div>
+    
+  );
 }
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer>FOOTER WILL GO HERE</footer>;
+  return <footer class="page-footer footer-light bg-light"> 
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="https://codaisseur.com/"> codaisseur.com</a>
+    </div>  
+  </footer>;
 }
